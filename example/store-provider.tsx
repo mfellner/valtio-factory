3abff685a7f6e6a407d9e1ce8a8eaedffb0b0b7b
Maximilian Fellner <mail@mfellner.eu>
2022-01-30 01:25:38 +0100

    docs: update example

--- example/store-provider.tsx
+++ example/store-provider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, PropsWithChildren, useContext } from 'react';
+import React, { createContext, PropsWithChildren, useContext, useEffect } from 'react';
 import { RootStore } from './model';
 
 const storeContext = createContext<RootStore | null>(null);
@@ -6,6 +6,15 @@ const storeContext = createContext<RootStore | null>(null);
 type StoreProviderProps = PropsWithChildren<{ rootStore: RootStore }>;
 
 export function StoreProvider({ rootStore, children }: StoreProviderProps) {
+  // Unsubscribe from all updates on unmount.
+  useEffect(() => {
+    return () => {
+      rootStore.counter.$unsubscribe();
+      rootStore.user.$unsubscribe();
+      rootStore.$unsubscribe();
+    };
+  }, [rootStore]);
+
   return <storeContext.Provider value={rootStore}>{children}</storeContext.Provider>;
 }
 
